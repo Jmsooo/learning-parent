feb94013087061edbffcf533df54d623360d495c
jmye <jiaming_06@163.com>
2023-09-27 22:07:48 +0800

    spring 整合 log4j2

--- spring6-parent/spring6-first/src/test/java/com/jmye/spring6/first/HelloWorldTest.java
+++ spring6-parent/spring6-first/src/test/java/com/jmye/spring6/first/HelloWorldTest.java
@@ -2,6 +2,8 @@ package com.jmye.spring6.first;
 
 import com.jmye.spring6.first.beans.HelloWorld;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,12 +14,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @Version 1.0
  */
 public class HelloWorldTest {
+    private Logger logger = LoggerFactory.getLogger(HelloWorldTest.class);
 
     @Test
     public void testHelloWorld() {
         ApplicationContext ac = new ClassPathXmlApplicationContext("spring-beans.xml");
         HelloWorld helloworld = (HelloWorld) ac.getBean("helloWorld");
         helloworld.sayHello();
+        logger.info("执行成功...");
     }
 
 }
