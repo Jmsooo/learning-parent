78715fff25202c14ba6ac3ecad9b08c5329469aa
jmye <jiaming_06@163.com>
2023-11-05 23:44:39 +0800

    获取 Nacos 服务列表

--- cloud-alibaba-parent/sca-03-provider-nacos-8083/src/main/java/com/jmye/cloud/controller/DepartController.java
+++ cloud-alibaba-parent/sca-03-provider-nacos-8083/src/main/java/com/jmye/cloud/controller/DepartController.java
@@ -3,6 +3,8 @@ package com.jmye.cloud.controller;
 import com.jmye.cloud.bean.Depart;
 import com.jmye.cloud.service.DepartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Description
@@ -26,6 +30,27 @@ public class DepartController {
 
     @Autowired
     private DepartService departService;
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @GetMapping("/discovery")
+    public List<String> discovery() {
+        List<String> services = client.getServices();
+        for (String serviceName : services) {
+            List<ServiceInstance> instances = client.getInstances(serviceName);
+            for (ServiceInstance instance : instances) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("serviceName", serviceName);
+                map.put("serviceId", instance.getInstanceId());
+                map.put("host", instance.getHost());
+                map.put("port", instance.getPort());
+                map.put("uri", instance.getUri());
+                System.out.println(map);
+            }
+        }
+        return services;
+    }
 
     @PostMapping("/save")
     public boolean save(@RequestBody Depart depart) {
