a07e39008e9dd3a36c94534cf32bee31973570a8
jmye <jiaming_06@163.com>
2023-11-07 23:38:19 +0800

    Gateway 配置路由

---
+++ cloud-alibaba-parent/sca-08-gateway-config-9001/src/main/java/com/jmye/cloud/GatewayConfigApplication.java
@@ -0,0 +1,14 @@
+package com.jmye.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class GatewayConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayConfigApplication.class, args);
+    }
+}
